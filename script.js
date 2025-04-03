db1965e185cbc2c4aac20fb953d9cafc26227c3c
crmSaaS <crmsaas.catination.com>
2025-04-03 22:29:09 +0530

    first day

---
+++ script.js
@@ -0,0 +1,9 @@
+// function buy(){
+//     alert("bought")
+// }
+function buy(){
+    document.getElementById("hero").style.display="none";
+    document.getElementById("hero2").style.display="block";
+    
+
+}
